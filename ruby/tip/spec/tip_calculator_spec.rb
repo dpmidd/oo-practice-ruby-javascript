cd5a1c38629b48f25cab2f4d8398652b261d5d1a
Dmidd87 <dmidd87@gmail.com>
2015-03-07 17:22:17 -0700

    Added test for amount owed

--- ruby/tip/spec/tip_calculator_spec.rb
+++ ruby/tip/spec/tip_calculator_spec.rb
@@ -23,5 +23,15 @@ RSpec.describe TipCalculator do
     expect(tip2.cost). to eq(18.00)
   end
 
+  it "A person owes a certain amount for a tip" do
+    tip = TipCalculator.new(4.44)
+    tip.owes("Sebastian", 4.44)
 
+    expect(tip.cost).to eq(8.88)
+
+    tip2 = Tip.calculator.new(2.22)
+    tip.owes("Bart", 1.11)
+
+    expect(tip.cost).to eq(3.33)
+  end
 end
